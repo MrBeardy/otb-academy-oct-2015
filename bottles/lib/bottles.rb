c5b1f92811e142f982c56189da465bb82bb62e33
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2015-09-28 14:22:06 +0100

    Start working on sean's version.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,63 +1,52 @@
 class Bottles
   def verse(num)
-    "#{pluralize(num).capitalize} of beer on the wall, #{pluralize num} of beer.\n#{outcome num}, #{pluralize(next_num(num))} of beer on the wall.\n"
-  end
-
-  def sing
-    verses(max_num, 0)
-  end
-
-  def max_num
-    99
+      "#{no_more(num).capitalize} #{bottles_or_bottle(num)} of beer on the wall, #{no_more(num)} #{bottles_or_bottle(num)} of beer.\n" +
+        "#{action(num)}, #{bottles_remaining(num)} of beer on the wall.\n"
   end
 
   def verses(num, num2)
     result = ""
     num.downto(num2).each do |v|
-      result += self.verse(v) + "\n"
+      result += verse(v) + "\n"
     end
     result
   end
 
+  def sing
+    verses(99,0)
+  end
+
   private
-
-  def outcome(num)
-    if (num >= 1)
-      "Take #{article num} down and pass it around"
-    else
+  def action(num)
+    if num == 0
       "Go to the store and buy some more"
+    else
+      "Take #{it_or_one(num)} down and pass it around"
     end
   end
 
-  def article(num)
-    if (num > 1)
-      "one"
+  def bottles_or_bottle(num)
+    num == 1 ? "bottle" : "bottles"
+  end
+
+  def it_or_one(num)
+    num == 1 ? "it" : "one"
+  end
+
+  def bottles_remaining(num)
+    if num == 1
+      "no more bottles"
     else
-      "it"
+      "#{decrement(num)} #{bottles_or_bottle(num - 1)}"
     end
   end
 
-  def noun(num)
-    if (num == 1)
-      "bottle"
-    else
-      "bottles"
-    end
+  def decrement(num)
+    return 99 if num == 0
+    num - 1
   end
 
-  def next_num(num)
-    if (num < 1)
-      max_num
-    else
-      num - 1
-    end
-  end
-
-  def pluralize(num)
-    if (num == 0)
-      "no more #{noun num}"
-    else
-      "#{num} #{noun num}"
-    end
+  def no_more(num)
+    num == 0 ? "no more" : "#{num}"
   end
 end
