c636bcf1e1be22ed88de1be156febc0e841085c2
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2015-09-29 10:55:59 +0100

    Finish alarm project.

--- tyre-pressure/lib/alarm.rb
+++ tyre-pressure/lib/alarm.rb
@@ -1,3 +1,52 @@
+require 'sensor'
+
+class SensorLower < Sensor
+  def sample_pressure
+    15
+  end
+end
+
+class SensorHigher < Sensor
+  def sample_pressure
+    25
+  end
+end
+
+class SensorPassing < Sensor
+  def sample_pressure
+    18
+  end
+end
+
 class Alarm
-  # Fill in the implementation here
+  attr_accessor :sensor
+
+  def initialize( sensor )
+    @sensor = sensor
+    @is_on = false
+  end
+
+  def to_s
+    if on?
+      "( ! )"
+    else
+      ""
+    end
+  end
+
+  def check()
+    pressure = @sensor.sample_pressure
+
+    if (17.5 .. 21).include? pressure
+      @is_on = false
+    else
+      @is_on = true
+    end
+
+    pressure
+  end
+
+  def on?
+    @is_on
+  end
 end
--- tyre-pressure/spec/alarm_spec.rb
+++ tyre-pressure/spec/alarm_spec.rb
@@ -1,4 +1,56 @@
 require 'alarm'
+
+#
+# the alarm doesnt turn on if it's within the range
+# the alarom does turn on if it's outside the range
+
 RSpec.describe "tyre pressure alarm" do
-  it "needs some tests"
+  it "should check can retrieve a float from the sensor object" do
+    sensor = Sensor.new
+    expect( Alarm.new( sensor ).check ).to be_a(Float)
+  end
+
+  it "should initially be off" do
+    sensor = Sensor.new
+    expect( Alarm.new(sensor).on? ).to be false
+  end
+
+  it "doesnt turn on if it's within the range" do
+    sensor = SensorPassing.new
+    alarm = Alarm.new( sensor )
+    alarm.check
+
+    expect( alarm.on? ).to be false
+  end
+
+  it "turns on when outside range, and back off when inside range" do
+    alarm = Alarm.new( SensorHigher.new )
+    alarm.check
+
+    expect( alarm.on? ).to be true
+
+    alarm.sensor = SensorPassing.new
+    alarm.check
+
+    expect( alarm.on? ).to be false
+  end
+
+  it "turns the alarm on if it's outside of the range" do
+    alarm = Alarm.new( SensorHigher.new )
+    alarm.check
+
+    expect( alarm.on? ).to be true
+
+    alarm = Alarm.new( SensorLower.new )
+    alarm.check
+
+    expect( alarm.on? ).to be true
+  end
+
+  it "shows a TPMS low pressure warning icon when pressure is outside of range" do
+    alarm = Alarm.new( SensorHigher.new )
+    alarm.check
+
+    expect( alarm.to_s ).to eq "( ! )"
+  end
 end
